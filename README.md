9ff4e55615c5e63489d79ba09c309817e70ffa14
Angus McNair <amcnair234@gmail.com>
2020-03-05 15:30:18 -0600

    Added additional projects

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio-Site
+This is a personal portfolio website for Angus McNair, created using HTML, CSS, JavaScript and jQuery.
---
+++ index.js
@@ -0,0 +1,35 @@
+$(() => {
+    
+    //Expand mobile menu upon clicking hamburger icon
+    $(".hamburger").click(function () {
+        if ($("nav").hasClass(".show-menu")) {
+            $("nav").removeClass(".show-menu").fadeOut(250);
+        }
+        else {
+            $("nav").addClass('.show-menu').fadeIn(250);
+        }
+    });
+
+    //Auto-closes mobile menu after a link has been clicked.
+    $("li").click(function () {
+        if ($(window).width() < 800){
+        $("nav").removeClass(".show-menu").fadeOut(250); 
+        }
+    });
+
+    //Ensure menu appears when screen is resized
+    //(Fixes an issue in which after the hamburger menu was opened 
+    //and closed, then changed to desktop size, menu would disappear)
+    $(window).resize(function () {
+        if ($(window).width() >= 800) {
+            $("nav").show();
+        } else ($("nav").hide())
+    });
+
+    //Scrolls to portfolio on click of button
+    $(".portfolio-button").click(function (){
+        $("html, body").animate({
+            scrollTop: $("hr").offset().top
+        }, 1000)
+        })
+});
